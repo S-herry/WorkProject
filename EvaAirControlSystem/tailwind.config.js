da73906d890910a6359b8d0af4cf255c26781feb
DESKTOP-PFLL5I2\golde <hight7501@gmail.com>
2025-03-07 21:18:55 +0800

    長榮環控

---
+++ EvaAirControlSystem/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        MineBgColor: "#001321",
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ EvaAirControlSystem/vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  optimizeDeps: {
+    include: ["esm-dep > cjs-dep"],
+  },
+  base: "",
+  plugins: [react()],
+  publicDir: "static",
+  build: {
+    outDir: "build",
+    manifest: true,
+    // ssrManifest: true,  // 取得每個資源預載指令
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/js/[name].js",
+        assetFileNames: (assetInfo) => {
+          const ext = assetInfo.names[0].split(".").pop();
+          if (ext) {
+            // 判斷檔案名稱
+            if (["png", "jpg", "jpeg", "gif", "svg"].includes(ext)) {
+              return `assets/images/[name].[ext]`;
+            } else if (["css"].includes(ext)) {
+              return `assets/css/[name].[ext]`;
+            } else if (["woff", "woff2", "ttf", "otf"].includes(ext)) {
+              return `assets/fonts/[name].[ext]`;
+            } else if (["json"].includes(ext)) {
+              return `assets/data/[name].[ext]`;
+            }
+            return `assets/other/[name].[ext]`;
+          }
+          return "assets/other/[name].[ext]";
+        },
+        chunkFileNames: `assets/js/[name].js`,
+      },
+    },
+  },
+  server: {
+    open: false, // 可以是路由 string/boolean
+    host: true,
+  },
+});
